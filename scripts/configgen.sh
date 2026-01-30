ad369f36b5b6e74521a4da5bac82f90b6b7f279f
ak474000 <ak474000@vivaldi.net>
2026-01-30 01:21:41 -0500

    Initial commit

--- scripts/configgen.sh
+++ scripts/configgen.sh
@@ -3,48 +3,24 @@
 configGen(){
 if [ $REGEN_CONFIG = "true" ]; then
 	echo "Generating config JSON...."
-	jq -n \
-  	  --arg name "$SERVER_NAME" \
-  	  --arg motd "$MOTD" \
-  	  --arg pass "$PASSWORD" \
-  	  --argjson max "$MAX_PLAYERS" \
-  	  --argjson radius "$MAX_RADIUS" \
-  	  --arg world "$WORLD_NAME" \
-  	  --arg mode "$GAME_MODE" \
-  '{
-    	"Version": 3,
-    	"ServerName": $name,
-    	"MOTD": $motd,
-    	"Password": $pass,
-    	"MaxPlayers": $max,
-    	"MaxViewRadius": $radius,
-    	"LocalCompressionEnabled": false,
-    	"Defaults": {
-      	"World": $world,
-      	"GameMode": $mode
-    	},
-    	"ConnectionTimeouts": {
-      	"JoinTimeouts": {}
-    	},
-    	"RateLimit": {},
-    	 "Modules": {
-      	  "PathPlugin": {
-           "Modules": {}
-      	}
-    	},
-    	"LogLevels": {},
-    	"Mods": {},
-    	"DisplayTmpTagsInStrings": false,
-    	"PlayerStorage": {
-      	  "Type": "Hytale"
-    	},
-    	  "AuthCredentialStore": {
-      	  "Type": "Encrypted",
-      	  "Path": "auth.enc"
-    	 }
-     }' > config.json
-	echo -e "config generated.\n"
 
+	cat config.json | \
+	jq --arg sname "$SERVER_NAME" \
+  	  --arg motd "$MOTD" \
+  	  --arg passw "$PASSWORD" \
+  	  --argjson maxp "$MAX_PLAYERS" \
+  	  --argjson radius "$MAX_RADIUS" \
+  	  --arg worldn "$WORLD_NAME" \
+  	  --arg gmode "$GAME_MODE"  \
+	'.ServerName |= $sname | .MOTD |= $motd | .Password |= $passw | .MaxPlayers |= $maxp | .MaxViewRadius |= $radius | .Defaults.World |= $worldn | .Defaults.GameMode |= $gmode' > config.tmp && mv config.tmp config.json
+	
+	local testauth=$(jq config.json '.AuthCredentialStore.Type')
+
+	if [ $testauth  != "Encrypted" ]; then
+		cat config.json | jq '. += [{"AuthCredentialStore"{"Type": "Encrypted", "Path": "auth.enc"}}]' > config.tmp && mv config.tmp config.json
+	fi
+
+	echo -e "config generated.\n"
 else
   echo -e "Skipping config JSON generation.\n"
 fi

7335505cd5ba67d76850b85674d121d2d61f2abb
ak474000 <ak474000@vivaldi.net>
2026-02-01 02:47:00 -0500

    Inital commit

---
+++ scripts/updater.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+updateCheck(){
+    local currentver
+    local jarver
+    if [ -f $JARFILE ]; then
+        
+        currentver=$(echo "v$(./ht-downloader -print-version)")
+        jarver=$(java -jar $JARFILE --version | cut -d ' ' -f 2)
+
+        if (( $currentver > $jarver )); then
+            echo -e "New Version Avilable updating Server JAR....\n"
+        fi
+
+    else
+
+    fi
+
+
+}

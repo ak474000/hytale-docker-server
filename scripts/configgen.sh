071b8ae49f46e8d14c9bba32fdb36dfd6b6d2361
ak474000 <ak474000@vivaldi.net>
2026-02-03 23:05:06 -0500

    updated configGen

--- scripts/configgen.sh
+++ scripts/configgen.sh
@@ -44,66 +44,6 @@ if [ $REGEN_CONFIG = "true" ]; then
     	 }
      }' > config.json
 	echo -e "config generated.\n"
-	
-	# world config seeding
-	if [ ! -d "universe" ]; then
-		mkdir universe/worlds/${WORLD_NAME}
-	fi
-	jq -n \
-  	  --argjson max "$" \
-  	  --arg mode "$" \
-	'{
-  	"Version": 4,
-  	"UUID": {
-    	"$binary": "bUBOwTlMRXq1Dmllz5ERgg==",
-    	"$type": "04"
- 	 },
-  	"Seed": ,
-  	"WorldGen": {
-    	"Type": "Hytale",
-    	"Name": "Default"
-  	},
-  	"WorldMap": {
-    	"Type": "WorldGen"
-  	},
-  	"ChunkStorage": {
-    	"Type": "Hytale"
-  	},
-  	"ChunkConfig": {},
-  	"IsTicking": true,
-  	"IsBlockTicking": true,
-  	"IsPvpEnabled": false,
-  	"IsFallDamageEnabled": true,
-  	"IsGameTimePaused": false,
-  	"GameTime": "0001-01-09T11:53:05.741520798Z",
-  	"ClientEffects": {
-    	"SunHeightPercent": 100.0,
-    	"SunAngleDegrees": 0.0,
-    	"BloomIntensity": 0.30000001192092896,
-    	"BloomPower": 8.0,
-    	"SunIntensity": 0.25,
-    	"SunshaftIntensity": 0.30000001192092896,
-    	"SunshaftScaleFactor": 4.0
-  	},
-  	"RequiredPlugins": {},
-  	"IsSpawningNPC": true,
-  	"IsSpawnMarkersEnabled": true,
-  	"IsAllNPCFrozen": false,
-  	"GameplayConfig": "Default",
-  	"IsCompassUpdating": true,
-  	"IsSavingPlayers": true,
-  	"IsSavingChunks": true,
-  	"SaveNewChunks": true,
-  	"IsUnloadingChunks": true,
-  	"IsObjectiveMarkersEnabled": true,
-  	"DeleteOnUniverseStart": false,
-  	"DeleteOnRemove": false,
-  	"ResourceStorage": {
-    	"Type": "Hytale"
-  	},
-  	"Plugin": {}
-	}' > ./universe/worlds/${WORLD_NAME}/config.json
-
 else
   echo -e "Skipping config JSON generation.\n"
 fi
